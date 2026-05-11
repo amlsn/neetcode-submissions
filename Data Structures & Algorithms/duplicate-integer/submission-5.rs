69c09a7172047087c08bc365bb96873c742c0107
Amlan Sahoo <amlan.sahoo.98@gmail.com>
2026-05-11 18:31:47 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.rs
@@ -0,0 +1,13 @@
+use std::collections::HashSet;
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut track_set = HashSet::new();
+        for i in 0..nums.len() {
+            if track_set.contains(&nums[i]) {
+                return true;
+            }
+            track_set.insert(nums[i]);
+        }
+        false
+    }
+}
